6cd776a47243fc668b6697fe0986d5b7a7e01b20
PetrIvChe <cherniavskiipetr@gmail.com>
2021-12-07 22:20:41 +0300

    Preapared for tests:build.gradle. After, each

---
+++ src/test/java/ru/netology/web/CarOrderCallbackTest.java
@@ -0,0 +1,38 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class CarOrderCallbackTest {
+    private ChromeDriver driver;
+    private ChromeOptions options;
+
+
+    @BeforeAll
+    public static void setUpAll() {
+
+
+        System.setProperty("webdriver.chrome.driver", "./driver/win32/chromedriver.exe")
+    }
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+}
+
+}
